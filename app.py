dbd1dcec8e9409004042499c54c78f3f31f1511c
snpierce <n.pierce@yale.edu>
2021-12-30 17:43:09 -0500

    progress

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, session, url_for, g
 from flask_session import Session
-from helpers import search_apology, find_words, clean_list
+from helpers import search_apology, find_words, clean_list, get_letters, calculate_ml, generate_apology, get_db
 
 # Configure application
 app = Flask(__name__)
@@ -12,6 +12,12 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.after_request
 def after_request(response):
@@ -32,9 +38,13 @@ def solve():
     
     if request.method == "POST":
         search = (request.form.get("pangram")).lower()
+        midletter = (request.form.get("midletter")).lower()
 
         if len(search) != 7:
             return search_apology("Incorrect amount of letters.")
+
+        if len(midletter) > 1:
+            return search_apology("Enter one middle letter, or enter none to produce all possible words.")
 
         abcs = []
         for letter in search:
@@ -43,7 +53,10 @@ def solve():
             else:
                 return search_apology("Please enter unique letters.")
         
-        results = find_words(search, abcs)
+        if midletter not in abcs:
+            return search_apology("Middle letter not found in entered letter set.")
+        
+        results = find_words(abcs, midletter)
         
         return clean_list(results, abcs)
 
@@ -54,6 +67,81 @@ def solve():
         return render_template("solve.html", perf_pangrams=perf_pangrams, pangrams=pangrams, words=words)
 
 
-@app.route("/generate")
+@app.route("/generate", methods=["GET", "POST"])
 def generate():
-    return render_template("generate.html")
+
+    if request.method == "POST":
+
+        random = get_letters()
+
+        return redirect(url_for("generate_play", search=random))
+    else:
+        return render_template("generate.html")
+
+        
+@app.route("/generate/<search>", methods=["GET", "POST"])
+def generate_play(search):
+    cur = get_db().cursor()
+
+    if request.method == "POST":
+
+        if not request.form.get("guess"):
+            return generate_apology("Please enter guess.")
+        
+        guess = request.form.get("guess")
+
+        if len(guess) < 4:
+            return generate_apology("Too short.")
+
+        
+        cur.execute("SELECT * FROM search")
+        info = cur.fetchone()
+        search = info[0]
+        points = info[1]
+        midletter = info[2]
+        max_p = info[3]
+
+        if midletter not in guess:
+            return generate_apology("Missing middle letter.")
+
+        for letter in guess:
+            if letter not in search:
+                return generate_apology("Includes non-valid letters.")
+        
+        cur.execute("SELECT * FROM list")
+        wlist = list(cur.fetchall())
+        cur.execute("SELECT * FROM guessed")
+        guessed = list(cur.fetchall())
+
+        if guess in wlist:
+            if guess not in guessed:
+                cur.execute("INSERT INTO guessed (word) Values (?)", (guess,))
+                get_db().commit()
+            else:
+                return generate_apology("Already found.")
+        else:
+            return generate_apology("Not in word list.")
+        
+        cur.execute("SELECT * FROM guessed")
+        guessed = list(cur.fetchall())
+
+        return render_template("play.html", letters=search, midletter=midletter, max=max_p, points=points, correct=guessed)
+
+    else:
+        midletter, max_p = calculate_ml(search)
+        wlist = find_words(search, midletter)
+        guessed = []
+        
+        cur.execute("DELETE FROM search")
+        cur.execute("DELETE FROM guessed")
+        cur.execute("DELETE FROM list")
+        cur.execute("INSERT INTO search (search, points, midlet, max_p) Values (?, ?, ?, ?)", (search, 0, midletter, max_p,))
+
+        for word in wlist:
+            cur.execute("INSERT INTO list (word) Values (?)", (word,))
+        get_db().commit()
+
+        cur.execute("SELECT points FROM search")
+        points = cur.fetchone()[0]
+
+        return render_template("play.html", letters=search, midletter=midletter, max=max_p, points=points)
--- helpers.py
+++ helpers.py
@@ -1,4 +1,12 @@
-from flask import render_template
+from flask import render_template, g
+import random
+import sqlite3
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect("pangrams.sqlite", check_same_thread=False)
+    return db
 
 def load_words():
     dictionary = []
@@ -13,7 +21,19 @@ def load_words():
 def search_apology(message, code=400):
     return render_template("search_apology.html", message=message), code
 
-def find_words(search, abcs):
+def generate_apology(message, code=400):
+    cur = get_db().cursor()
+
+    cur.execute("SELECT * FROM search")
+    info = cur.fetchone()
+    search = info[0]
+    points = info[1]
+    midletter = info[2]
+    max_p = info[3]
+
+    return render_template("play.html", letters=search, midletter=midletter, max=max_p, points=points, message=message)
+
+def find_words(abcs, midletter):
     
     dictionary = load_words()
     search_results = []
@@ -24,6 +44,8 @@ def find_words(search, abcs):
             if letter not in abcs:
                 mark = False
         if mark == False:
+            continue
+        if midletter not in word:
             continue
         search_results.append(word)
 
@@ -45,7 +67,69 @@ def clean_list(results, letters):
     results.sort()
     results = sorted(results, key=len)
 
-    return render_template("solve.html", perf_pangrams=perf_pangrams, pangrams=pangrams, words=results)
+    cur_len = 4
+    results_by_length = []
+    temp = []
+
+    for word in results:
+        if len(word) > cur_len:
+             results_by_length.append(temp)
+             cur_len = len(word)
+             temp = []
+        else:
+            temp.append(word)
+
+    results_by_length.append(temp)
+
+    return render_template("solve.html", perf_pangrams=perf_pangrams, pangrams=pangrams, words=results_by_length)
 
 def remove_dup(str): 
     return "".join(set(str))
+
+def get_letters():
+    pangrams = []
+    with open('pangrams.txt') as file:
+        valid_words = set(file.read().split())
+        for word in valid_words:
+            pangrams.append(word)
+
+    num = random.randint(0, len(pangrams))
+
+    return pangrams[num]
+
+
+def calculate_ml(search):
+    lengths = []
+    for letter in search:
+        temp = find_words(search, letter)
+        lengths.append(len(temp))
+
+    min_count = lengths[0]
+    min_index = 0
+    max_count = lengths[0]
+    max_index = 0
+
+    for i in range(7):
+        if lengths[i] < min_count:
+            min_count = lengths[i]
+            min_index = i
+        elif lengths[i] > max_count:
+            max_count = lengths[i]
+            max_index = i
+
+    if max_count < 50:
+        return search[max_index], lengths[max_index]
+
+    if min_count > 75:
+        return search[min_index], lengths[min_index]
+
+    return search[3], lengths[3]
+
+
+    
+
+
+
+
+
+
